bc97ab304b781ff1f3658e446226731f7bac5552
Jagriti <jagriti.agni@gmail.com>
2020-11-05 03:01:17 +0530

    [Jagriti]ADDED: Test case to check if the salary updated for multiple employees is in sync with DB

--- src/test/java/com/bl/jdbc/employeepayrollservicejdbc/EmployeePayrollTest.java
+++ src/test/java/com/bl/jdbc/employeepayrollservicejdbc/EmployeePayrollTest.java
@@ -6,7 +6,9 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,6 +22,7 @@ public class EmployeePayrollTest {
 				.readEmployeePayrollDataDB(EmployeePayrollService.IOService.DB_IO);
 		Assert.assertEquals(14, empPayrollData.size());
 	}
+
 //
 //	@Test
 	public void givenNewSalaryForEmployee_whenUpdated_shouldSyncWithDB() {
@@ -27,7 +30,7 @@ public class EmployeePayrollTest {
 		List<EmployeePayrollData> empPayrollData = empPayrollService
 				.readEmployeePayrollDataDB(EmployeePayrollService.IOService.DB_IO);
 		empPayrollService.updateEmployeeSalary("Bella", 2000000.0);
-		
+
 		boolean result = empPayrollService.checkEmployeePayrollInSyncWithDB("Bella");
 		Assert.assertTrue(result);
 	}
@@ -51,9 +54,10 @@ public class EmployeePayrollTest {
 		String gender = "F";
 		double avgSalary = empPayrollService.getEmpDataAvgByGender(gender);
 		System.out.println("avg -" + avgSalary);
-		boolean result = (2333333.0 == avgSalary);
+		boolean result = (1028571.0 == avgSalary);
 		Assert.assertTrue(result);
 	}
+
 //
 //	UC 9-11 -- implement ERD
 	@Test
@@ -68,9 +72,9 @@ public class EmployeePayrollTest {
 	// multi-threading
 
 	@Test
-	public void given6Employee_WhenAddedDatabase_ShouldMatchEntries() throws EmployeePayrollException {
+	public void givenSixEmployee_whenAddedDatabase_shouldMatchEntries() throws EmployeePayrollException {
 		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(11, "Sam", "F", 100000.0, LocalDate.now()),
-				new EmployeePayrollData(0, "Harry", "F", 600000.0, LocalDate.now()),
+				new EmployeePayrollData(0, "Harry", "M", 600000.0, LocalDate.now()),
 				new EmployeePayrollData(0, "Roohi", "F", 700000.0, LocalDate.now()),
 				new EmployeePayrollData(0, "Nandini", "F", 800000.0, LocalDate.now()),
 				new EmployeePayrollData(0, "Ram", "M", 900000.0, LocalDate.now()),
@@ -88,5 +92,24 @@ public class EmployeePayrollTest {
 		Instant threadEnd = Instant.now();
 		System.out.println("Duration with Thread: " + Duration.between(threadStart, threadEnd));
 		Assert.assertEquals(14, employeePayroll.countEntries());
+	}
+
+	@Test
+	public void givenNewSalaryForMultipleEmployees_whenUpdated_shouldSyncWithDB() {
+		EmployeePayrollService empPayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> empPayrollData = empPayrollService
+				.readEmployeePayrollDataDB(EmployeePayrollService.IOService.DB_IO);
+		Map<String, Double> nameToSalaryMap = new HashMap<>();
+		nameToSalaryMap.put("Harry", 400000.0);
+		nameToSalaryMap.put("Shyam", 900000.0);
+		nameToSalaryMap.put("Ram", 500000.0);
+		Instant threadStart = Instant.now();
+		empPayrollService.updateEmployeeSalary(nameToSalaryMap);
+		boolean result = empPayrollService.checkEmployeePayrollInSyncWithDB("Harry")
+				&& empPayrollService.checkEmployeePayrollInSyncWithDB("Shyam")
+				&& empPayrollService.checkEmployeePayrollInSyncWithDB("Ram");
+		Instant threadEnd = Instant.now();
+		System.out.println("Duration with Thread: " + Duration.between(threadStart, threadEnd));
+		Assert.assertTrue(result);
 	}
 }
